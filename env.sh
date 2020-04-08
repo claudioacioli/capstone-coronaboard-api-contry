6a9ae2dc1d9cbc64ce913b62668d43c06790e3f8
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-04-07 23:54:06 -0300

    chore: Configue environments variables

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export FLASK_APP=main.py
+export POSTGRES_HOST=172.17.0.1
+export POSTGRES_PORT=5432
+export POSTGRES_USER=coronaboard
+export POSTGRES_NAME=coronaboard
+export POSTGRES_PASS=123456
